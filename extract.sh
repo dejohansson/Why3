6c0af38428c6eed246d963d929291d990da7f14f
dejohansson <davjom-5@student.ltu.se>
2018-10-21 18:41:44 +0200

    Added AST optimization

--- extract.sh
+++ extract.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "why3 extract --recursive --modular -D ocaml64 -D ocaml64_bv.drv *.mlw -o ../../ocaml/extract -L ."
-why3 extract --recursive --modular -D ocaml64 -D ocaml64_bv.drv *.mlw -o ../../ocaml/extract -L .
+echo "why3 extract --recursive --modular -D ocaml64 -D ocaml64_bv.drv *.mlw -o ../cimp_2018/extract -L ."
+why3 extract --recursive --modular -D ocaml64 -D ocaml64_bv.drv *.mlw -o ../cimp_2018/extract -L .
